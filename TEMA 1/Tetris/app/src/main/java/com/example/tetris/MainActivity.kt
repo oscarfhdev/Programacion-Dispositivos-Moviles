c0f071918e84b13d7dffd2b7cd9676a0f1a4fcfc
oscarfhdev <oscarfh.dev@gmail.com>
2025-10-23 23:46:50 +0200

    UPDATED TEMA1/Tetris --> finished, tetris with 1 square functional

--- TEMA 1/Tetris/app/src/main/java/com/example/tetris/MainActivity.kt
+++ TEMA 1/Tetris/app/src/main/java/com/example/tetris/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         val rectanguloView: View = findViewById(R.id.rectangulo);
 
-        val rectangulo: Rectangulo = Rectangulo(ContextCompat.getColor(this, R.color.cyan), 100, 100)
+        val rectangulo: Rectangulo = Rectangulo(ContextCompat.getColor(this, R.color.cyan), 200, 200)
 
         val btnArriba: Button = findViewById(R.id.btnArriba)
         val btnAbajo: Button = findViewById(R.id.btnAbajo)
@@ -26,6 +26,56 @@ class MainActivity : AppCompatActivity() {
         val btnDerecha: Button = findViewById(R.id.btnDerecha)
         val btnCambiarTamanio: Button = findViewById(R.id.btnCambiarTamanio)
         val btnCambiarColor: Button = findViewById(R.id.btnCambiarColor)
+
+        btnArriba.setOnClickListener {
+            rectangulo.movArriba()
+            actualizarVista(rectangulo, rectanguloView)
+        }
+
+        btnAbajo.setOnClickListener {
+            rectangulo.movAbajo()
+            actualizarVista(rectangulo, rectanguloView)
+        }
+
+        btnIzquierda.setOnClickListener {
+            rectangulo.movIzqda()
+            actualizarVista(rectangulo,rectanguloView)
+
+        }
+
+        btnDerecha.setOnClickListener {
+            rectangulo.movDcha()
+            actualizarVista(rectangulo,rectanguloView)
+
+        }
+
+        btnCambiarTamanio.setOnClickListener {
+            rectangulo.cambiarTamano(200, 200)
+            actualizarVista(rectangulo,rectanguloView)
+
+        }
+
+        btnCambiarColor.setOnClickListener {
+            rectangulo.color = ContextCompat.getColor(this, R.color.black)
+            actualizarVista(rectangulo,rectanguloView)
+        }
+
+    }
+
+    private fun actualizarVista(rectangulo: Rectangulo, rectanguloView: View){
+
+        // Al rectanguloView la da el valor del rectangulo clase Rectangulo
+        rectanguloView.layoutParams.width = rectangulo.ancho
+        rectanguloView.layoutParams.height = rectangulo.alto
+
+        rectanguloView.setBackgroundColor(rectangulo.color)
+
+        rectanguloView.x = rectangulo.x.toFloat()
+        rectanguloView.y = rectangulo.y.toFloat()
+
+        // método para que tome efecto, solicita de nuevo el layout
+        rectanguloView.requestLayout()
+
     }
 
 }
